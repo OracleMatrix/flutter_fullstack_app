ed6d685f5f014abcfa4398d5afd1d8ca483245d5
Ehsan <47227409+OracleMatrix@users.noreply.github.com>
2025-04-15 20:24:03 +0330

    server URL

--- lib/app/data/constants.dart
+++ lib/app/data/constants.dart
@@ -1,6 +1,6 @@
 import 'package:get_storage/get_storage.dart';
 
 class Constants {
-  static String baseUrl = 'https://4295-45-86-202-94.ngrok-free.app';
+  static String baseUrl = 'http://localhost:8000';
   static GetStorage storage = GetStorage();
 }
